a2504745f8931dda8a4dbf41fa2ff09f7b2046d2
ehichioyaodion <ehichioyaodion@gmail.com>
2025-03-25 02:15:51 +0000

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './screens/**/*.{js,jsx,ts,tsx}', // Add this line to include the screens directory
+  ],
 
   presets: [require('nativewind/preset')],
   theme: {
